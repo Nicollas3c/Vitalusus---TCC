adfd9a2e3a61bba49bcb625b95552db052a4a94a
OttoFidelis <ottofidelis@gmail.com>
2023-11-18 21:47:12 -0300

    fiz mais um ajuste no código

--- VitalususPlus/src/main/java/com/vitalususPlus/VitalususPlus/VitalususPlusApplication.java
+++ VitalususPlus/src/main/java/com/vitalususPlus/VitalususPlus/VitalususPlusApplication.java
@@ -9,6 +9,7 @@ public class VitalususPlusApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VitalususPlusApplication.class, args);
 		String pessoa = System.getProperty("user.name");
+		pessoa = pessoa.toUpperCase();
 		System.out.println("TRABALHA, " + pessoa + "!");
 	}
 
