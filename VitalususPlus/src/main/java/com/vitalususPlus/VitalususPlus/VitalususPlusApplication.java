875509b81140b5a65ab9e48aeede30ad37bf6ae7
OttoFidelis <ottofidelis@gmail.com>
2023-11-18 22:00:16 -0300

    fiz mais ajusts ainda no vitalusus

--- VitalususPlus/src/main/java/com/vitalususPlus/VitalususPlus/VitalususPlusApplication.java
+++ VitalususPlus/src/main/java/com/vitalususPlus/VitalususPlus/VitalususPlusApplication.java
@@ -5,7 +5,42 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VitalususPlusApplication {
-
+	
+	public static void main1(String[] args) {
+		SpringApplication.run(VitalususPlusApplication.class, args);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VitalususPlusApplication.class, args);
 		String pessoa = System.getProperty("user.name");
