c124a82a799340ee82c46f2af3163106cdc08d3d
OttoFidelis <ottofidelis@gmail.com>
2024-04-07 18:14:31 -0300

    adicionei o findById no UsuarioController

--- VitalususPlus/src/main/java/br/itb/projeto/vitalususPlus/rest/controller/UsuarioController.java
+++ VitalususPlus/src/main/java/br/itb/projeto/vitalususPlus/rest/controller/UsuarioController.java
@@ -1,12 +1,19 @@
 package br.itb.projeto.vitalususPlus.rest.controller;
 
+import br.itb.projeto.vitalususPlus.model.entity.Usuario;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.itb.projeto.vitalususPlus.service.UsuarioService;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/Vitalusus/usuario")
+@RequestMapping("/vitalusus/usuario/")
 public class UsuarioController {
 	private UsuarioService usuarioService;
 
@@ -14,5 +21,14 @@ public class UsuarioController {
 		super();
 		this.usuarioService = usuarioService;
 	}
-	
+	@GetMapping("findAll")
+	public ResponseEntity<List<Usuario>> findAll(){
+		List<Usuario> usuarios = this.usuarioService.findAll();
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+	}
+	@GetMapping("findById/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable long id){
+		Usuario usuario = this.usuarioService.findById(id);
+		return  new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+	}
 }
