963241d2c7fc200a35170e30f3e547050634cdf3
Otto Fidelis <ottofidelis@gmail.com>
2024-04-16 12:54:41 -0300

    invocamos o Kennedy

--- Vitalusus/src/App.jsx
+++ Vitalusus/src/App.jsx
@@ -7,29 +7,29 @@ function App() {
     setEgua(egua-1)
     if (egua<=0) setEgua(0)
   }
-  let eguaString = ' POCOTÓ RESTANTE'
+  let eguaString = ' QI RESTANTE'
   let mensagem = ''
   let imagem = ''
   let imagemCavalo = document.getElementById('pocoto')
 
   if (egua==0){
-    mensagem = 'Jumpscare de égua!'
-    imagem = 'https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia16043/caracteristicas-cavalos-saudaveis-artigos-cursos-cpt.jpg'
+    mensagem = 'Kennedy Invocado!... O que você fez!?'
+    imagem = 'kennedy2-removebg-preview.png'
     imagemCavalo.className='cavalo'
   }
   
   if (egua>1 || egua==0)
-  eguaString=' POCOTÓS RESTANTES'
+  eguaString=' QI RESTANTE'
 
 
   return(
   <div className='App'>
-    <h1>INVOCAÇÃO DE ÉGUAS</h1>
+    <h1>COMO INVOCAR UM KENNEDY, TUTORIAL ATUALIZADO 2024</h1>
     <h3>{egua}{eguaString}</h3>
     <h3>{mensagem}</h3>
     <img id='pocoto' src={imagem}></img>
     <br/>
-    <button onClick={removeEgua} className='btn btn-primary'>POCOTÓ</button>
+    <button onClick={removeEgua} className='btn btn-primary'>USAR DORGAS</button>
   </div>
   )
 }
